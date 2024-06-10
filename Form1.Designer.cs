dce0a9bea5d639292f49a3ead92421ec7954f3ff
quy2222008 <voquocquy23032022@gmail.com>
2024-06-10 16:51:05 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Csharp_12congiap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btntim = new System.Windows.Forms.Button();
+            this.lblnam = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtduong = new System.Windows.Forms.TextBox();
+            this.txtamlich = new System.Windows.Forms.TextBox();
+            this.lblketqua = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btntim
+            // 
+            this.btntim.AllowDrop = true;
+            this.btntim.Location = new System.Drawing.Point(211, 322);
+            this.btntim.Name = "btntim";
+            this.btntim.Size = new System.Drawing.Size(87, 54);
+            this.btntim.TabIndex = 0;
+            this.btntim.Text = "tìm";
+            this.btntim.UseVisualStyleBackColor = true;
+            this.btntim.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblnam
+            // 
+            this.lblnam.AutoSize = true;
+            this.lblnam.Location = new System.Drawing.Point(145, 89);
+            this.lblnam.Name = "lblnam";
+            this.lblnam.Size = new System.Drawing.Size(27, 13);
+            this.lblnam.TabIndex = 1;
+            this.lblnam.Text = "năm";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(410, 56);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(350, 355);
+            this.listBox1.TabIndex = 2;
+            // 
+            // txtduong
+            // 
+            this.txtduong.Location = new System.Drawing.Point(211, 89);
+            this.txtduong.Name = "txtduong";
+            this.txtduong.Size = new System.Drawing.Size(100, 20);
+            this.txtduong.TabIndex = 3;
+            // 
+            // txtamlich
+            // 
+            this.txtamlich.Location = new System.Drawing.Point(211, 163);
+            this.txtamlich.Name = "txtamlich";
+            this.txtamlich.Size = new System.Drawing.Size(100, 20);
+            this.txtamlich.TabIndex = 5;
+            // 
+            // lblketqua
+            // 
+            this.lblketqua.AutoSize = true;
+            this.lblketqua.Location = new System.Drawing.Point(145, 170);
+            this.lblketqua.Name = "lblketqua";
+            this.lblketqua.Size = new System.Drawing.Size(43, 13);
+            this.lblketqua.TabIndex = 4;
+            this.lblketqua.Text = "kết quả";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtamlich);
+            this.Controls.Add(this.lblketqua);
+            this.Controls.Add(this.txtduong);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblnam);
+            this.Controls.Add(this.btntim);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btntim;
+        private System.Windows.Forms.Label lblnam;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtduong;
+        private System.Windows.Forms.TextBox txtamlich;
+        private System.Windows.Forms.Label lblketqua;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_12congiap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+                    string can, chi;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 2000; i <= 2050; i++)
+            {
+                int indexCan = i % 10;
+                int indexChi = i % 12;
+               
+            }
+           
+          
+            switch (Convert.ToInt32(txtduong.Text) % 10) 
+            {
+                case 0:
+                    can = "canh";
+                    break;
+                case 1:
+                    can = "tan";
+                    break;
+                case 2:
+                    can = "nham";
+                    break;
+                case 3:
+                    can = "quy";
+                    break;
+                case 4:
+                    can = "giap";
+                    break;
+                case 5:
+                    can = "at";
+                    break;
+                case 6:
+                    can = "binh";
+                    break;
+                case 7:
+                    can = "dinh";
+                    break;
+                case 8:
+                    can = "mau";
+                    break;
+
+                case 9:
+                    can = "ky";
+                    break;
+
+                
+                
+
+            }
+            switch (Convert.ToInt32(txtduong.Text) % 12)
+            {
+                case 0:
+                    chi = "thân";
+                    break;
+                case 1:
+                    chi = "dậu";
+                    break;
+                case 2:
+                    chi = "tuất";
+                    break;
+                case 3:
+                    chi = "hợi";
+                    break;
+                case 4:
+                    chi = "tý";
+                    break;
+                case 5:
+                    chi = "sửu";
+                    break;
+
+                case 6:
+                    chi = "dần";
+                    break;
+                case 7:
+                    chi = "mão";
+                    break;
+
+                case 8:
+                    chi = "thìn";
+                    break;
+
+                case 9:
+                    chi = "tỵ";
+                    break;
+
+
+                case 10:
+                    chi = "ngọ";
+                    break;
+                case 11:
+                    chi = "mùi";
+                    break;
+
+
+                    txtamlich.Text = can + " " + chi;
+            }
+            ;
+        }
+    }
+}
